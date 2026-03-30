0df82fbceb0dbb251d5715071bd8ef5f9e2fb686
Viktoriya Gagarina <viktoriya.gagarina@eltex-co.ru>
2026-03-30 10:03:06 +0700

    Add mark for initialization

--- Project/Sources/Entity/Book.swift
+++ Project/Sources/Entity/Book.swift
@@ -15,6 +15,8 @@ struct Book: Codable {
     private(set) var name: String!
     private(set) var experationDate: Date!
     
+    // MARK: - Init
+
     init?(id: String?,
           name: String?,
           experationDate: Date?) {
