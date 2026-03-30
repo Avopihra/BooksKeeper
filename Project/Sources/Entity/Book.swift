48a7b72001fd022748c367736ed50e85db2b8094
Viktoriya Gagarina <viktoriya.gagarina@eltex-co.ru>
2026-03-30 10:02:34 +0700

    Add mark for properties

--- Project/Sources/Entity/Book.swift
+++ Project/Sources/Entity/Book.swift
@@ -38,6 +38,7 @@ struct Book: Codable {
         self.experationDate = experationDate
     }
     
+    // MARK: - Public Properties
     var isExpired: Bool {
         let timeInSeconds = Date().timeIntervalSince(self.experationDate)
         if timeInSeconds > (60*60*24) {
